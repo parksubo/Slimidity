40b803c47be8d62e379c4be07a15a4886257bd00
yogjin <youngjin988@gmail.com>
2022-05-13 18:01:43 +0900

    style: 버튼 클릭 시 색 변경

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,39 +1,56 @@
-import React, { FC } from 'react';
+import React, { FC, MouseEvent, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
 
 const Header: FC = ({ children }) => {
+  const [clicked, setClicked] = useState([false, false, false]);
+
+  const changeButtonColor = (num: number) => {
+    let newClicked = [false, false, false];
+    newClicked[num] = true;
+    setClicked(newClicked);
+  };
   return (
-    <>
-      <div className={styles.container}>
-        <div className={styles.header}>
-          <Link to="/">
-            <button className={`${styles.headerBtn} ${styles.logoBtn}`}>
-              로고 이미지 추가예정
-            </button>
-          </Link>
-          <Link to="/Marketplace">
-            <button className={`${styles.headerBtn} ${styles.MarketplaceBtn}`}>
-              Marketplace
-            </button>
-          </Link>
-          <Link to="/Breeding">
-            <button className={`${styles.headerBtn} ${styles.BreedingBtn}`}>
-              Breeding
-            </button>
-          </Link>
-          <Link to="/Game">
-            <button className={`${styles.headerBtn} ${styles.gameBtn}`}>
-              Game
-            </button>
-          </Link>
-          <button className={`${styles.headerBtn} ${styles.loginBtn}`}>
-            Login
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <Link to="/">
+          <button className={`${styles.headerBtn} ${styles.logoBtn}`}>
+            로고 이미지 추가예정
           </button>
-        </div>
-        <div className={styles.children}>{children}</div>
+        </Link>
+        <Link
+          to="/Marketplace"
+          className={clicked[0] ? styles.clickedLink : ''}
+        >
+          <button
+            className={`${styles.headerBtn} ${styles.MarketplaceBtn}`}
+            onClick={() => changeButtonColor(0)}
+          >
+            Marketplace
+          </button>
+        </Link>
+        <Link to="/Breeding" className={clicked[1] ? styles.clickedLink : ''}>
+          <button
+            className={`${styles.headerBtn} ${styles.MarketplaceBtn}`}
+            onClick={() => changeButtonColor(1)}
+          >
+            Breeding
+          </button>
+        </Link>
+        <Link to="/Game" className={clicked[2] ? styles.clickedLink : ''}>
+          <button
+            className={`${styles.headerBtn} ${styles.MarketplaceBtn}`}
+            onClick={() => changeButtonColor(2)}
+          >
+            Game
+          </button>
+        </Link>
+        <button className={`${styles.headerBtn} ${styles.loginBtn}`}>
+          Login
+        </button>
       </div>
-    </>
+      <div className={styles.children}>{children}</div>
+    </div>
   );
 };
 
