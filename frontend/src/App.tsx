1c44aa6bf2b35af7ac9e515b6eaeb7c71b6b4676
yogjin <youngjin988@gmail.com>
2022-05-13 19:01:27 +0900

    feat: Marketplace Page init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import React, { FC } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Main from './routes/Main/Main';
+import Marketplace from './routes/Marketplace/Marketplace';
 const App: FC = () => {
   return (
     <BrowserRouter>
       <Header>
         <Routes>
           <Route path="/" element={<Main />} />
+          <Route path="/Marketplace" element={<Marketplace />} />
         </Routes>
       </Header>
     </BrowserRouter>
---
+++ frontend/src/routes/Marketplace/Marketplace.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+import styles from './Marketplace.module.css';
+
+const Marketplace: FC = (props) => {
+  return (
+    <div className={styles.rootContainer}>
+      <div className={styles.left}>dd</div>
+      <div className={styles.right}>ss</div>
+    </div>
+  );
+};
+
+export default Marketplace;
