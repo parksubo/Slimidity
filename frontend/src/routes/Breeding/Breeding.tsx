7d18bda5591cf66136eaec09173f0c8bb35c447e
yogjin <youngjin988@gmail.com>
2022-05-18 01:41:43 +0900

    feat: card 클릭 시 breeding 장소로 이동 기능
    1. card object type aliases 2. breeding 장소에 있는 card 클릭시 다시 card list로 돌아가는 기능도 포함

--- frontend/src/routes/Breeding/Breeding.tsx
+++ frontend/src/routes/Breeding/Breeding.tsx
@@ -1,10 +1,49 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import NFTCard from '../../components/Breeding/NFTCard';
 import styles from './Breeding.module.css';
 
+function isEmptyObj(obj: Object): boolean {
+  if (obj === undefined) return true;
+  if (obj.constructor === Object && Object.keys(obj).length === 0) {
+    return true;
+  }
+  return false;
+}
+
+// Type Aliases
+type NFT = {
+  id: string;
+  type: string;
+  attack: number;
+};
+
 const Breeding: FC = () => {
-  const nfts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]; // 추후 contract에서 nft받아오기
-  const clicked = [0, 0]; // 선택한 카드
+  const [nfts, setNfts] = useState<Array<NFT>>([
+    { id: '1', type: 'ice', attack: 1 },
+    { id: '2', type: 'fire', attack: 2 },
+    { id: '3', type: 'wind', attack: 4 },
+    { id: '4', type: 'ice', attack: 3 },
+  ]); // 추후 contract에서 nft받아오기
+  const [clicked, setClicked] = useState<Array<NFT>>([]); // 선택한 카드
+
+  const onClickCard = (e: React.MouseEvent<HTMLInputElement>) => {
+    const id: string = e.currentTarget.getAttribute('data-id')!;
+    const nft: NFT = nfts.find((nft) => nft.id === id)!;
+
+    let newClicked: Array<NFT> = [...clicked];
+    if (newClicked.length < 2) {
+      newClicked.push(nft);
+    }
+    setClicked(newClicked);
+  };
+
+  const onClickBreedingCard = (e: React.MouseEvent<HTMLInputElement>) => {
+    const id: string = e.currentTarget.getAttribute('data-id')!;
+    let newClicked: Array<NFT> = clicked.filter((nft) => nft.id !== id);
+
+    setClicked(newClicked);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.left}>
@@ -12,8 +51,13 @@ const Breeding: FC = () => {
           <span>카드 두개를 선택해서 브리딩을 수행해보세요</span>
         </div>
         <div className={styles.cards}>
-          {nfts.map((nft) => (
-            <div className={styles.card}>
+          {nfts.map((nft, idx) => (
+            <div
+              className={styles.card}
+              key={idx}
+              data-id={nft.id}
+              onClick={onClickCard}
+            >
               <NFTCard />
             </div>
           ))}
@@ -24,16 +68,32 @@ const Breeding: FC = () => {
           <span>브리딩 장소</span>
         </div>
         <div className={styles.breedingPlace}>
-          <div className={styles.clickedCard}>
-            <NFTCard />
-          </div>
+          {isEmptyObj(clicked[0]) ? (
+            <div className={styles.clickedCard}></div>
+          ) : (
+            <div
+              className={styles.clickedCard}
+              onClick={onClickBreedingCard}
+              data-id={clicked[0].id}
+            >
+              <NFTCard />
+            </div>
+          )}
           <img
             src={require('../../images/heart.png')}
             className={styles.heartImg}
           />
-          <div className={styles.clickedCard}>
-            <NFTCard />
-          </div>
+          {isEmptyObj(clicked[1]) ? (
+            <div className={styles.clickedCard}></div>
+          ) : (
+            <div
+              className={styles.clickedCard}
+              onClick={onClickBreedingCard}
+              data-id={clicked[1].id}
+            >
+              <NFTCard />
+            </div>
+          )}
         </div>
         <div className={styles.breedingBtn}>
           <button className="btn btn-outline-secondary">브리딩 하기</button>
