29a0e9df14b1538f01bb97ff90c58e760f08a22c
yogjin <youngjin988@gmail.com>
2022-05-27 02:35:57 +0900

    feat(GeneScience): slime type(타입) 결정 함수 추가

--- contracts/GeneScience.sol
+++ contracts/GeneScience.sol
@@ -30,6 +30,41 @@ contract GeneScience is Constants {
         return string(newGene);
     }
     
+    // father mother 유전자 받아서 타입 리턴: bytes1
+    function mixGeneReturnType(string memory _fatherGene, string memory _motherGene) internal view returns (string memory) {
+        bool a = isGeneScience(_fatherGene);
+        bool b = isGeneScience(_motherGene);
+        require(a&&b, "Invalid gene");
+
+        bytes memory _fatherGeneBytes = bytes(_fatherGene);
+        bytes memory _motherGeneBytes = bytes(_motherGene);
+        // 0이면 father 1이면 mother 2이면 mutation(미구현)
+        //string memory newType;
+        bytes1 tempType;
+        string memory newType;
+
+        // 타입 결정, 0이면 father, 1이면 mother 유전자에 따른 타입 결정
+        uint256 randomNumber = uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender, MAX))) % 2;  
+        if(randomNumber == 0) {
+            tempType = _fatherGeneBytes[2];
+        }
+        else {
+            tempType = _motherGeneBytes[2];
+        }
+
+        if(tempType == "0") {
+            newType = "green";
+        }
+        else if(tempType == "1") {
+            newType = "pink";
+        }
+        else { 
+            newType = "blue";
+        }
+
+        return newType;
+    }
+
     // bytes로 유전자 정보를 받아 길이가 9인지 확인하는 함수
     function isGeneScience(string memory _genes) internal pure returns (bool) {
         bytes memory byteGenes = bytes(_genes);
