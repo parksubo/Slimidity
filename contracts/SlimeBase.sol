e1fd6ff4e8eddbf8b57657e5bf3b3df74281a0d4
yogjin <youngjin988@gmail.com>
2022-05-27 03:32:09 +0900

    refactor(SlimeBase): 사용하지 않는 함수, 변수 삭제

--- contracts/SlimeBase.sol
+++ contracts/SlimeBase.sol
@@ -32,10 +32,8 @@ contract SlimeBase is ERC721Enumerable, GeneScience {
 
     // key: ownerAddress => value: tokenId
     mapping(address => uint256[]) public ownersTokenIds;
-    // key: ownerAddress => (key: tokenId => value: tokenIdAtIndex)
-    mapping(address => mapping(uint256 => int256)) public ownersTokenIdAtIndex;
     // 특정 슬라임의 유전자를 가지는 슬라임이 몇 개인지 카운트
-    mapping(string => uint256) public slimeCountPerGene;
+    // mapping(string => uint256) public slimeCountPerGene;
 
     constructor() ERC721('Slimes', 'SLME') {
         baseUri = '';
@@ -50,28 +48,6 @@ contract SlimeBase is ERC721Enumerable, GeneScience {
     // 배포자의 잔액 확인 (external로 다큰 컨트랙트나 트랜잭션을 통해서만 호출)
     function getContractBalance() external view returns (uint256) { //onlyDeployer returns (uint256) {
         return address(this).balance;
-    }
-    /*
-    // baseURI Get
-    function _baseURI() internal view virtual override returns (string memory) {
-        return baseUri;
-    }
-    // baseURI Set
-    function setBaseUri(string memory _baseUri) external onlyDeployer {
-        baseUri = _baseUri;
-    }
-    */
-    
-    // 
-    function addOwnerShip (address _owner, uint256 tokenId) internal {
-        require(_owner != address(0x0), "Owner must a valid address");
-
-        // 소유자 주소에 tokenId 추가 
-        ownersTokenIds[_owner].push(tokenId);
-        // 토큰 생성 번호
-        int256 tokenIndex = int256(ownersTokenIds[_owner].length - 1);
-        // [소유자][토큰ID]에 소유자의 몇번째 소유 토큰인지를 기록
-        ownersTokenIdAtIndex[_owner][tokenId] = tokenIndex;
     }
 
     // 슬라임 생성 후 토큰Id 리턴
@@ -101,14 +77,12 @@ contract SlimeBase is ERC721Enumerable, GeneScience {
 
         // 소유자 계정으로 토큰민팅
         _mint(_owner, newTokenId);
-        // 소유자에게 소유권 부여
-        addOwnerShip(_owner, newTokenId);
         // 슬라임 배열에 생성된 슬라임 추가
         slimes.push(_slime);
         // 생성했으므로 카운터 트래커 인덱스++
         slimeIndexTracker.increment();
         // 해당 유전자를 가진 슬라임 개수++
-        slimeCountPerGene[_genes] += 1;
+        // slimeCountPerGene[_genes] += 1;
 
         return newTokenId;
     }
