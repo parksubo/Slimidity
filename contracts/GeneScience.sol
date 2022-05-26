5fca4644706d3332e3e04637645c5a75c6867c40
yogjin <youngjin988@gmail.com>
2022-05-27 02:27:47 +0900

    refactor: GeneScience 컨트랙트 주석 삭제

--- contracts/GeneScience.sol
+++ contracts/GeneScience.sol
@@ -2,13 +2,9 @@
 // remixd -s . --remix-ide https://remix.ethereum.org
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-
 contract Constants {
     uint256 internal constant HUNDRED = 100;
     uint256 internal constant GENE_LENGTH = 9;
-    //uint256 internal constant GENE_ATTRS = 3;
-    //uint256 internal constant MUTATION_CHANCE = 30; // 30%
 }
 
 contract GeneScience is Constants {
@@ -30,53 +26,9 @@ contract GeneScience is Constants {
         else {
             newGene = "001100010";
         }
-        /*
-        bytes memory crossoveredGene = new bytes(GENE_LENGTH);
-        uint256 divider = GENE_LENGTH / 2;
-        //  Just do a simple crossover gene mixing algorithm
-        for (uint256 i = 0; i < divider; i++) {
-            crossoveredGene[i + divider + 1] = _fatherGeneBytes[i];
-        }
 
-        for (uint256 i = divider; i < GENE_LENGTH; i++) {
-            crossoveredGene[i - divider] = _motherGeneBytes[i];
-        }
-        int256 where = 0;
-        int256 attr = 0;
-
-        (where, attr) = mutation();
-
-        //  There is a mutation!
-        if (where != -1) {
-            string memory attrStr = uint2str(uint256(attr));
-            bytes memory bytesAttrStr = bytes(attrStr);
-
-            crossoveredGene[uint256(where)] = bytesAttrStr[0];
-        }
-        return string(crossoveredGene);
-        */
         return string(newGene);
     }
-
-    /*
-    //  Apply mock mutation for only 15% chance
-    function mutation() internal view returns (int256, int256) {
-        
-        uint256 randomHash = uint256(keccak256(abi.encodePacked(block.difficulty, block.timestamp, HUNDRED)));
-
-        uint256 randomNumber = randomHash % HUNDRED;
-        bool isMutation = randomNumber < MUTATION_CHANCE;
-
-        if (isMutation) {         
-            uint256 where = uint256(keccak256(abi.encodePacked(block.difficulty, block.timestamp, HUNDRED)));
-            uint256 attr = uint256(keccak256(abi.encodePacked(block.difficulty, block.timestamp, HUNDRED)));
-
-            return (int256(where % GENE_LENGTH), int256(attr % GENE_ATTRS));
-        }
-
-        return (-1, -1);
-    }
-    */
     
     // bytes로 유전자 정보를 받아 길이가 9인지 확인하는 함수
     function isGeneScience(string memory _genes) internal pure returns (bool) {
